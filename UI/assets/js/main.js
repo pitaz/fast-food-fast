7825e502384c8ec60d33c804801198670d004b55
Peter Odekwo <peter.odekwo@gmail.com>
2018-09-15 10:29:23 +0100

    Fixed side navigation error in user's ordered food page

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,20 +1,11 @@
 const contentarea = document.getElementsByClassName('contentarea')[0];
 const sidebar = document.getElementsByClassName('sidebar')[0];
 const toggle = document.getElementsByClassName('menu-icon')[0];
-const adminToggle = document.getElementsByClassName('menu-icon-admin')[0];
 
 const toggleSideNav = () => {
   sidebar.classList.toggle('active');
   contentarea.classList.toggle('active');
 };
 
-const toggleAdminNav = () => {
-  sidebar.classList.toggle('active');
-  contentarea.classList.toggle('active');
-};
+toggle.addEventListener('click', toggleSideNav);
 
-if (toggle) {
-  toggle.addEventListener('click', toggleSideNav);
-} else {
-  adminToggle.addEventListener('click', toggleAdminNav);
-}
