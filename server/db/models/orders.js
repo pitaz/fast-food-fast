3aeb367104e6237634d943c86a959bfd84fa82af
Peter Odekwo <peter.odekwo@gmail.com>
2018-09-30 19:24:32 +0100

    Chore:Setup database
    Installed pg
    created db models
    [Delivered #160871100]

---
+++ server/db/models/orders.js
@@ -0,0 +1,21 @@
+import pgclient from '..';
+
+
+const createOrdersTB = (client) => {
+  const query = `
+    DROP TABLE IF EXISTS "orders" CASCADE;
+    CREATE TABLE "orders" (
+        "id" serial PRIMARY KEY,
+        "name" VARCHAR(50) NOT NULL,
+        "userId" INTEGER REFERENCES "users" ("id"),
+        "quantity" INTEGER NOT NULL,
+        "status" VARCHAR(20) NOT NULL,
+        "dateCreated" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );`;
+
+  client.query(query)
+    .then(res => res)
+    .catch(e => e.message);
+};
+
+createOrdersTB(pgclient);
---
+++ server/db/models/users.js
@@ -0,0 +1,28 @@
+import bcrypt from 'bcryptjs';
+import pgclient from '../index';
+
+
+const passwordHash = bcrypt.hash(process.env.ADMIN_PASS, 10);
+const email = process.env.ADMIN_EMAIL;
+const name = process.env.ADMIN_NAME;
+
+const createUsersTB = (client) => {
+  const query = `
+    DROP TABLE IF EXISTS "users" CASCADE;
+    CREATE TABLE "users" (
+        "id" serial PRIMARY KEY,
+        "role" VARCHAR(50) NOT NULL,
+        "name" VARCHAR(50) NOT NULL,
+        "email" VARCHAR(100) UNIQUE NOT NULL,
+        "password" VARCHAR(100) NOT NULL,
+        "dateCreated" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
+    INSERT INTO "users" (role, name, email, password)
+    VALUES ('admin','${name}', '${email}','${passwordHash}')`;
+
+  client.query(query)
+    .then(res => res)
+    .catch(e => e.message);
+};
+
+createUsersTB(pgclient);
