418620eb7bd7a5ee38077ddb6a32b87f21f7feb3
Peter Odekwo <peter.odekwo@gmail.com>
2018-10-02 17:28:47 +0100

    Modified package file

--- server-api/src/db/dbConnection.js
+++ server-api/src/db/dbConnection.js
@@ -3,11 +3,12 @@ import { Pool } from 'pg';
 
 dotenv.config();
 let sslValue;
-if (process.env === 'production') {
+if (process.env.NODE_ENV === 'production') {
   sslValue = true;
 } else {
   sslValue = false;
 }
+
 const dbclient = new Pool({
   connectionString: process.env.PG_DB_CONNECTION_URL, ssl: sslValue,
 });
