6646822623af4b992a672827c22c2d6459b5e00f
Peter Odekwo <peter.odekwo@gmail.com>
2018-10-02 09:45:08 +0100

    * Create a route for user registration * Create route for user registration * Write tests for Endpoint
    [#160877488](https://www.pivotaltracker.com/story/show/160877488)

--- server-api/src/db/dbConnection.js
+++ server-api/src/db/dbConnection.js
@@ -2,9 +2,14 @@ import dotenv from 'dotenv';
 import { Pool } from 'pg';
 
 dotenv.config();
-
+let sslValue;
+if (process.env === 'production') {
+  sslValue = true;
+} else {
+  sslValue = false;
+}
 const dbclient = new Pool({
-  connectionString: process.env.PG_DB_CONNECTION_URL,
+  connectionString: process.env.PG_DB_CONNECTION_URL, ssl: sslValue,
 });
 
 export default dbclient;
