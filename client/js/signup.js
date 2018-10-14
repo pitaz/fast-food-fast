1c7911eb6c82bee35810508a6ae5a1a3a3ed9b98
Peter Odekwo <peter.odekwo@gmail.com>
2018-10-14 10:48:33 +0100

    feat:connected signup page to backend
    - Created a signup.js file and utilized fetch API
    - Redirect user on successful login
    
    [finishes #161200674]

---
+++ client/js/signup.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-undef */
+const errorMsg = document.getElementById('error-message');
+const closeBtn = document.getElementById('closebtn');
+const errorMsgs = document.querySelectorAll('.error');
+const { form } = document.forms;
+const {
+  name, email, password, cpassword
+} = form.elements;
+
+const close = () => {
+  closeBtn.parentElement.style.display = 'none';
+};
+
+const confirmPasswordError = () => {
+  errorMsgs[2].classList.add('display-error');
+  errorMsgs[2].innerHTML = 'password does not match';
+};
+
+
+const redirectUser = () => {
+  window.location.href = '/food-menu.html';
+};
+
+const registerUser = (e) => {
+  e.preventDefault();
+
+  if (password.value === cpassword.value) {
+    const data = {
+      name: name.value,
+      email: email.value,
+      password: password.value
+    };
+
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    };
+
+    const url = '/api/v1/auth/signup';
+
+    fetch(url, options)
+      .then((res) => {
+        if (res.status === 409) {
+          closeBtn.parentElement.style.display = 'block';
+          errorMsg.innerText = 'Email already exist!';
+        }
+        return res.json();
+      })
+      .then((res) => {
+        if (res.status === 'success') {
+          localStorage.setItem('token', res.data.token);
+          localStorage.setItem('username', res.data.name);
+          redirectUser();
+        }
+
+        if (res.status === 'fail') {
+          closeBtn.parentElement.style.display = 'block';
+          errorMsg.innerText = res.message;
+        }
+      })
+      .catch(err => err.message);
+  } else {
+    confirmPasswordError();
+  }
+};
+
+form.addEventListener('submit', registerUser);
+closeBtn.addEventListener('click', close);
