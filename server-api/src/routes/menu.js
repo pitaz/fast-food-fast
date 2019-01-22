6ee4616105d1ea986aed8ef7350fe72d6a8cdb87
pitaz <peter.odekwo@gmail.com>
2019-01-22 16:35:04 +0100

    remove restricted access to menu route

--- server-api/src/routes/menu.js
+++ server-api/src/routes/menu.js
@@ -7,7 +7,7 @@ class Meals {
   meals(router) {
     router.route('/api/v1/menu')
       .post(authorize.user, authorize.admin, validateReq.validateMeal, mealsController.createMenu)
-      .get(authorize.user, mealsController.getMenu);
+      .get(mealsController.getMenu);
 
     router.route('/api/v1/menu/:id')
       .get(authorize.user, validateReq.validateId, mealsController.getMealById)
